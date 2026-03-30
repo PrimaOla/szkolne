687188473f3ca6660895dfb54923753e9a0e3ea0
Aleksandra Olszewska <s26660@pjwstk.edu.pl>
2026-03-30 16:38:40 +0200

    Add CalculateAverage method with int array parameter

--- SzkolneApp/StatisticsHelper.cs
+++ SzkolneApp/StatisticsHelper.cs
@@ -37,4 +37,21 @@ public class StatisticsHelper
         
         return numbers.Min();
     }
+
+        
+    public static double CalculateAverage(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+        
+        int sum = 0;
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+        
+        return (double)sum / values.Length;
+    }
 }
