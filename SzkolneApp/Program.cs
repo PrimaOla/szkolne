46d7a4f6b16c78eb7491b8c7db4e30f8bfb3b037
Aleksandra Olszewska <s26660@pjwstk.edu.pl>
2026-03-30 16:17:27 +0200

    Add user input handling for number collection

--- SzkolneApp/Program.cs
+++ SzkolneApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SzkolneApp;
+
+Console.WriteLine("=== Aplikacja do obliczania statystyk ===\n");
+
+List<double> numbers = new List<double>();
+
+Console.WriteLine("Podaj liczby do analizy (wpisz 'koniec' aby skończyć):");
+
+while (true)
+{
+    Console.Write("Liczba: ");
+    string input = Console.ReadLine() ?? "";
+    
+    if (input.ToLower() == "koniec")
+    {
+        break;
+    }
+    
+    if (double.TryParse(input, out double number))
+    {
+        numbers.Add(number);
+        Console.WriteLine($"✓ Dodano: {number}");
+    }
+}
+
+Console.WriteLine();
